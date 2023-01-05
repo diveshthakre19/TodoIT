8d38be0f918cecc5861c4690586abaadb5dc6440
diveshthakre19 <diveshthakre07@gmail.com>
2023-01-05 23:53:51 +0530

    add functionality

--- javascript/index.js
+++ javascript/index.js
@@ -28,7 +28,9 @@ const htmlTaskContent = ({ id, title, description, type, url }) => `
         <div class="card-footer"> 
             <button type="button" class="btn btn-outline-primary float-right"
               data-bs-toggel='modal'
-              data-bs-target='#showTask'>
+              data-bs-target='#showTask'
+              id=${id} 
+              onclick="openTask.apply(this, agruments)">
               Open Task 
             </button>
         </div>
@@ -40,14 +42,64 @@ const htmlModalContent = ({ id, title, description, url }) => {
   const date = new Date(parseInt(id));
   return `
   <div id=${id}>
-  ${url &&
+    ${url &&
     `<img width="100%" src=${url}  
-    alt="CardImage" 
-    class="img-fluid m-3 place_holder_image" />`
+      alt="CardImage" 
+        class="img-fluid m-3 
+        place_holder_image" />`
     }
-    <strong class="text-sm text-muted"> created on ${date.toDateString()}</strong>
-    <h2 class="my-3"> ${title} </h2>
-    <p class="Lead">${description} </p>
+      <strong class="text-sm text-muted"> created on ${date.toDateString()}</strong>
+        <h2 class="my-3"> ${title} </h2>
+      <p class="Lead">${description} </p>
   </div>
   `;
 };
+
+const updateLocalStorage = () => {
+  localStorage.setItem(tasks,
+    JSON.stringify({
+      tasks: state.taskList,
+    })
+  );
+};
+
+const loadInitalData = () => {
+  const localStorageCopy = JSON.parse(localStorage.tasks);
+  console.log(JSON.parse(localStorage.tasks))
+  if (localStorageCopy) state.taskList = localStorageCopy.tasks;
+
+  state.taskList.map((cardDate) => {
+    taskContents.insertAdjacentElement(
+      "beforebegin",
+      htmlTaskContent(cardDate)
+    );
+  });
+};
+
+const handelSubmit = (event) => {
+  const id = `${Date.now()}`;
+  const input = {
+    url: document.getElementById("imageURL").value,
+    title: document.getElementById("taskTitle").value,
+    description: document.getElementById("taskDescription").value,
+    type: document.getElementById("tags").value,
+  };
+
+
+  if (input.title === " " || input.description === " " || input.type === " ") {
+    return alert("Fill all fieds")
+  }
+
+  taskContents.insertAdjacentHTML(
+    "beforeend",
+    htmlTaskContent({ ...input, id })
+  );
+  state.taskList.push({ ...input, id });
+  updateLocalStorage();
+};
+
+const openTask = (e) => {
+  if (!e) e = window.event
+  const getTask = state.taskList.find(({ id }) => id === e.target.id)
+  taskModal.innerHTML = htmlModalContent(getTask)
+}
